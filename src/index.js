79f1950f5e5784627d66fd239d9530c80e2ef401
daswolke <wolke@weeb.sh>
2018-03-20 18:18:45 +0100

    add a service name

--- src/index.js
+++ src/index.js
@@ -99,7 +99,7 @@ let init = async() => {
     const server = app.listen(config.port, config.host);
     shutdownManager = new ShutdownHandler(server, registrator, null, pkg.name);
     if (registrator) {
-        await registrator.register(pkg.name, [config.env], config.port);
+        await registrator.register(pkg.serviceName, [config.env], config.port);
     }
     winston.info(`Server started on ${config.host}:${config.port}`);
     process.once('exit', () => {
